ac59674c36c9d9d1e05ec41f509a41f52fd789b0
cjgarde <cjgarde@gmail.com>
2019-02-01 16:44:32 +0100

    Remove not used views

--- src/views/DELETE-create-identity-wizard/components/index.js
+++
@@ -1,3 +0,0 @@
-export StepSetPassphrase from './step-set-passphrase/step-set-passphrase';
-export StepWelcome from './step-welcome/step-welcome';
-export StepSetLabel from './step-set-label/step-set-label';
--- src/views/DELETE-save-seed-wizard/components/index.js
+++
@@ -1,4 +0,0 @@
-export AskForPassphrase from './step-ask-for-passphrase/step-ask-for-passphrase';
-export AskForSeed from './step-ask-for-seed/step-ask-for-seed';
-export ShowQR from './step-show-qr/step-show-qr';
-export ShowSeed from './step-show-seed/step-show-seed';
