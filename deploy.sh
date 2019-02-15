6554dfaed1971405482ca4f4f880903e9ea89455
cjgarde <cjgarde@gmail.com>
2019-02-15 14:55:10 +0100

    create again keys

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 # exit with nonzero exit code if anything fails
 set -xe
-echo "--> DEPLOY TO ZOMBIE"
 
 if [ $TRAVIS_BRANCH == 'master' ] ; then
   echo "==> DEPLOY TO ZOMBIE"
+  # start an ssh-agent session
   eval "$(ssh-agent -s)"
+  #  adds the default keys ~/.ssh/id_rsa into the SSH authentication agent for implementing single sign-on with SSH
   ssh-add
   npm run build:prod
-  rsync -rq --delete --rsync-path="mkdir -p ~/www/zombie/ && rsync" \
-  $TRAVIS_BUILD_DIR/public travis@138.68.71.197:~/www/zombie/
+  rsync -rq --delete --rsync-path="mkdir -p ~/www/zombie/ && rsync" $TRAVIS_BUILD_DIR/public travis@138.68.71.197:~/www/zombie/
 else
   echo "==> Not deploying, since this branch isn't master."
 fi
